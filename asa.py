d3089f7560c876df425ffe6f8b14c69ec11a58b0
Sam Barnard <samcbar@gmail.com>
2018-04-02 17:35:03 -0700

    started asa.py

---
+++ asa.py
@@ -0,0 +1,44 @@
+
+# This library will handle ASA objects and policy
+
+#import pprint to view dictionary
+import pprint
+
+# going to need regex
+import re
+
+def get_asa_net_obj(asa_config):
+    "receives a text asa configuration file, returns a list with network object definitions"
+
+    '''
+    create a list for the objects
+    list because order for objects is not important
+       for converting between vendors
+    objects is a list of dictionaries, each entry in the list
+       is the name of the object
+    '''
+    objects = []
+    '''
+    asa objects look like this:
+
+    object network host1
+     host 192.12.45.66
+    object network 10.10.0.0-19
+     subnet 10.10.0.0 255.255.224.0
+     description Local summarized net
+    object network Microsoft-Update
+     fqdn v4 update.microsoft.com
+    '''
+
+
+
+
+    #return the list of objects
+    return objects
+
+
+f = open('asa.txt', 'r')
+
+print '\n'
+pprint.pprint(asa_obj_in(f))
+print '\n'
